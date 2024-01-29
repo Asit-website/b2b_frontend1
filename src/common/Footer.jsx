e118803b9f2ec9523e0ea7c6c7a390b108e56df5
asitmandal <asitmandal492@gmail.com>
2024-01-30 00:55:46 +0530

    this

--- src/common/Footer.jsx
+++ src/common/Footer.jsx
@@ -4,7 +4,8 @@ const Footer = () => {
   return (
     <>
        <div>
-        <h1>Foot</h1>
+        <h1 className='mt-3'>Foot</h1>
+        <button type="button" class="text-white bg-purple-700 hover:bg-purple-800 focus:outline-none focus:ring-4 focus:ring-purple-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900">Purple</button>
        </div>
      </>
   )
