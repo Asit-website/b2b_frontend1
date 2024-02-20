989d025c048ec6283e1dbbaacc27738fcb32a0fe
asitmandal <asitmandal492@gmail.com>
2024-02-21 04:45:41 +0530

    this

--- src/components/ProjectDetail.jsx
+++ src/components/ProjectDetail.jsx
@@ -4,138 +4,145 @@ import showMap from "../image/showMap.png"
 import Footer from "../common/Footer"
 import { useLocation } from "react-router-dom"
 import { useMain } from "../hooks/useMain";
-import { useEffect, useState   } from "react"
+import { useEffect, useState } from "react"
 
-    
 
-function ProjectDetail(){
+
+function ProjectDetail() {
 
     const { getProjectDetailById } = useMain();
 
     const location = useLocation();
     const projectId = location.state;
 
-    const [curentImg , setCurentImg] = useState(0);
+    const [curentImg, setCurentImg] = useState(0);
 
-    const [projectDetails , setProjectDetails] = useState();
+    const [projectDetails, setProjectDetails] = useState();
 
+    const [map, setMap] = useState(false);
+
+    const stylePeer = {
+        display: map ? "block" : "none"
+    }
     useEffect(() => {
 
         const fetchData = async () => {
-        
-          const ans = await getProjectDetailById({id: projectId});
-          console.log("ans" ,ans);
-          setProjectDetails(ans?.data);
-        }; 
 
-             fetchData();
+            const ans = await getProjectDetailById({ id: projectId });
+            console.log("ans", ans);
+            setProjectDetails(ans?.data);
+        };
+
+        fetchData();
         return () => {
         };
     }, []);
 
-   
+
 
     return (
 
         <div className="proDetWrap">
 
-            <div className="proDeCont">             
+            <div className="proDeCont">
 
-{/* top */}
-             <div className="proDeTop">
-            <h2 className="proTItl">{projectDetails?.title}</h2>
+                {/* top */}
+                <div className="proDeTop">
+                    <h2 className="proTItl">{projectDetails?.title}</h2>
 
-            <div className="topFlex">
+                    <div className="topFlex">
 
-                {/* left side  */}
-                <div className="proDetLeft">
+                        {/* left side  */}
+                        <div className="proDetLeft">
 
-                    {/* first */}
-                    <div className="prDTFir">
-                        <img src={projectDetails?.img[curentImg]?.url} alt="" />
-                    </div>
+                            {/* first */}
+                            <div className="prDTFir">
+                                <img src={projectDetails?.img[curentImg]?.url} alt="" />
+                            </div>
 
-                    {/* down */}
+                            {/* down */}
 
-                    <div className="prDTDwn">
-                        {
-                            projectDetails?.img?.map((item ,index)=>(
-                                <img onClick={()=>setCurentImg(index)} src={item?.url} key={index} />
-                            ))
-                        }
-                    </div>
+                            <div className="prDTDwn">
+                                {
+                                    projectDetails?.img?.map((item, index) => (
+                                        <img onClick={() => setCurentImg(index)} src={item?.url} key={index} />
+                                    ))
+                                }
+                            </div>
 
-                    {/* location */}
-                    <div className="proLocation">
+                            {/* location */}
+                            <div className="proLocation">
 
-                        <div className="locFirt">
+                                <div className="locFirt">
 
-                            <img src={mapPin} alt="" />
+                                    <img src={mapPin} alt="" />
 
-                            <div className="locTeeWrap">
-                                <p className="ltext">Location</p>
-                                <p className="reText">{projectDetails?.location}</p>
+                                    <div className="locTeeWrap">
+                                        <p className="ltext">Location</p>
+                                        <p className="reText">{projectDetails?.location}</p>
+                                    </div>
+
+                                </div>
+
+                                <button className="showMap" onClick={() => {
+                                    setMap(!map)
+                                }}>
+                                    <span>{map ? "Hide map" : "Show map"}</span>
+                                    <img src={showMap} alt="" />
+
+                                </button>
+
+                                <div style={stylePeer} className="mapRes">
+                                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d27858.716937287216!2d79.49755990514964!3d29.2135634298243!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39a09addbd0c86d1%3A0x6793e360cb3d930f!2sHaldwani%2C%20Uttarakhand%20263139!5e0!3m2!1sen!2sin!4v1708349361688!5m2!1sen!2sin" width="600" height="450" allowFullScreen loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
+                                </div>
+
                             </div>
 
                         </div>
 
-                         <button className="showMap">
-                             <span>Show map</span>
-                             <img src={showMap} alt="" />
+                        {/* right side  */}
+                        <div className="proDetRight">
 
-                         </button>
+                            {/* first  */}
+                            <div className="rpDetaSin">
+                                <p className="SiDtIL">Start Date </p>
+                                <p className="siAns">{projectDetails?.startDate}</p>
+                            </div>
 
-     <div className="mapRes">
-     <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d27858.716937287216!2d79.49755990514964!3d29.2135634298243!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39a09addbd0c86d1%3A0x6793e360cb3d930f!2sHaldwani%2C%20Uttarakhand%20263139!5e0!3m2!1sen!2sin!4v1708349361688!5m2!1sen!2sin" width="600" height="450"  allowFullScreen loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
-     </div>
+                            {/* first  */}
+                            <div className="rpDetaSin">
+                                <p className="SiDtIL">Bid Date  </p>
+                                <p className="siAns">{projectDetails?.bidDate}</p>
+                            </div>
+
+                            {/* first  */}
+                            <div className="rpDetaSin">
+                                <p className="SiDtIL">Stage  </p>
+                                <p className="siAns"> {projectDetails?.stage}</p>
+                            </div>
+
+                            {/* first  */}
+                            <div className="rpDetaSin">
+                                <p className="SiDtIL">Building Use  </p>
+                                <p className="siAns">{projectDetails?.buildingUse}</p>
+                            </div>
+
+                            {/* first  */}
+                            <div className="rpDetaSin">
+                                <p className="SiDtIL">Value  </p>
+                                <p className="siAns">{projectDetails?.value}</p>
+                            </div>
+
+                            {/* first  */}
+                            <div className="rpDetaSin">
+                                <p className="SiDtIL">Sector </p>
+                                <p className="siAns">{projectDetails?.sector}</p>
+                            </div>
+
+
+                        </div>
 
                     </div>
-            
-                </div>
-
-                {/* right side  */}
-                <div className="proDetRight">
-
-                     {/* first  */}
-                     <div className="rpDetaSin">
-                        <p className="SiDtIL">Start Date </p>
-                        <p className="siAns">{projectDetails?.startDate}</p>
-                     </div>
-
-                     {/* first  */}
-                     <div className="rpDetaSin">
-                        <p className="SiDtIL">Bid Date  </p>
-                        <p className="siAns">{projectDetails?.bidDate}</p>
-                     </div>
-
-                     {/* first  */}
-                     <div className="rpDetaSin">
-                        <p className="SiDtIL">Stage  </p>
-                        <p className="siAns"> {projectDetails?.stage}</p>
-                     </div>
-
-                     {/* first  */}
-                     <div className="rpDetaSin">
-                        <p className="SiDtIL">Building Use  </p>
-                        <p className="siAns">{projectDetails?.buildingUse}</p>
-                     </div>
-
-                     {/* first  */}
-                     <div className="rpDetaSin">
-                        <p className="SiDtIL">Value  </p>
-                        <p className="siAns">{projectDetails?.value}</p>
-                     </div>
-
-                     {/* first  */}
-                     <div className="rpDetaSin">
-                        <p className="SiDtIL">Sector </p>
-                        <p className="siAns">{projectDetails?.sector}</p>
-                     </div>
-
-
-                </div>
-
-                </div>
 
                 </div>
 
@@ -153,11 +160,11 @@ function ProjectDetail(){
             </div>
 
 
-<Footer />
+            <Footer />
 
         </div>
     )
 }
 
-export default ProjectDetail; 
+export default ProjectDetail;
 // apiKey:"AIzaSyCZRWpC8xfVQbIMlJ6upvOxBI6xQkERSvk"
--- src/context/MainState.jsx
+++ src/context/MainState.jsx
@@ -22,20 +22,20 @@ const MainState = (props) => {
     return data;
   };
 
-  const register = async ({ name, email, password,categoryies,confirmPassword }) => {
+  const register = async ({ name, email, password, categoryies, confirmPassword }) => {
     const resp = await fetch(`${baseUrl}/user/signin`, {
       method: 'POST',
       headers: {
         'content-type': 'application/json'
-        
+
       },
-      body: JSON.stringify({ name, email, password,categoryies,confirmPassword })
+      body: JSON.stringify({ name, email, password, categoryies, confirmPassword })
     });
     const data = await resp.json();
     return data;
   };
 
-  const getUsers = async (id,query, page, perPage) => {
+  const getUsers = async (id, query, page, perPage) => {
     const resp = await fetch(`${baseUrl}/user/getUsers?id=${id}&query=${query}&page=${page}&perPage=${perPage}`, {
       method: 'GET',
       headers: {
@@ -47,9 +47,9 @@ const MainState = (props) => {
     return data;
   };
 
-  const updateUser = async ({ userId, name, email, phone, categoryies,  website, budget, file, location,aboutCompany,twiter,facebook,linkdin,insta,city }) => {
+  const updateUser = async ({ userId, name, email, phone, categoryies, website, budget, file, location, aboutCompany, twiter, facebook, linkdin, insta, city }) => {
 
-    let formdata=new FormData();
+    let formdata = new FormData();
     formdata.append('name', name);
     formdata.append('email', email);
     formdata.append('phone', phone);
@@ -79,7 +79,7 @@ const MainState = (props) => {
     return data;
   };
 
-  const deleteImage = async({id , userId})=>{
+  const deleteImage = async ({ id, userId }) => {
     const token = localStorage.getItem('b2b_token');
 
 
@@ -163,9 +163,9 @@ const MainState = (props) => {
 
   // ! project APIS
 
-  const projectPostImage = async({file})=>{
-    let formdata=new FormData();
-  
+  const projectPostImage = async ({ file }) => {
+    let formdata = new FormData();
+
     formdata.append('file', file);
 
     const token = localStorage.getItem('b2b_token');
@@ -181,11 +181,11 @@ const MainState = (props) => {
 
     return data;
 
-   
+
   }
 
-  const projectDeleteImg = async(id)=>{
-  
+  const projectDeleteImg = async (id) => {
+
 
     const token = localStorage.getItem('b2b_token');
 
@@ -199,239 +199,239 @@ const MainState = (props) => {
 
     return data;
 
-   
+
   }
 
 
-  const postProject = async({title , desc , location , file ,userId , bidDate ,
+  const postProject = async ({ title, desc, location, file, userId, bidDate,
     startDate,
     stage,
     buildingUse,
     Value,
-    sector}) => {
+    sector }) => {
 
     const token = localStorage.getItem('b2b_token');
 
     const data = {
-        title,
-        desc,
-        location,
-        file , 
-        bidDate ,
-        startDate,
-        stage,
-        buildingUse,
-        Value ,
-        sector , 
+      title,
+      desc,
+      location,
+      file,
+      bidDate,
+      startDate,
+      stage,
+      buildingUse,
+      Value,
+      sector,
     };
 
 
     try {
-        const resp = await fetch(`${baseUrl}/project/postProject/${userId}`, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-                'Authorization': `Bearer ${token}`
-            },
-            body: JSON.stringify(data)
-        });
+      const resp = await fetch(`${baseUrl}/project/postProject/${userId}`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${token}`
+        },
+        body: JSON.stringify(data)
+      });
 
-        if (!resp.ok) {
-            throw new Error('Network response was not ok');
-        }
+      if (!resp.ok) {
+        throw new Error('Network response was not ok');
+      }
 
-        return await resp.json();
+      return await resp.json();
     } catch (error) {
-        console.error('There was an error!', error);
-        throw error;
+      console.error('There was an error!', error);
+      throw error;
     }
-};
+  };
 
 
-  const getProjects = async({userId}) => {
+  const getProjects = async ({ userId }) => {
     const token = localStorage.getItem('b2b_token');
-    
+
     let user1 = JSON?.parse(localStorage.getItem("b2b_user")) ? JSON?.parse(localStorage.getItem("b2b_user")) : null;
 
-    if(user1 === null){
+    if (user1 === null) {
       return alert("invalid token");
     }
-    
-     
+
+
     try {
-        const resp = await fetch(`${baseUrl}/project/getProjects/${user1._id}`, {
-            method: 'GET',
-            headers: {
-                'Authorization': `Bearer ${token}`
-            },
-        });
+      const resp = await fetch(`${baseUrl}/project/getProjects/${user1._id}`, {
+        method: 'GET',
+        headers: {
+          'Authorization': `Bearer ${token}`
+        },
+      });
 
-        if (!resp.ok) {
-            throw new Error('Network response was not ok');
-        }
+      if (!resp.ok) {
+        throw new Error('Network response was not ok');
+      }
 
-        return await resp.json();
+      return await resp.json();
     } catch (error) {
-        console.error('There was an error!', error);
-        throw error;
+      console.error('There was an error!', error);
+      throw error;
     }
-};
+  };
 
-const getProjects1 = async (id, query, page, perPage) => {
-  const resp = await fetch(`${baseUrl}/project/getProjects1?id=${id}&query=${query}&page=${page}&perPage=${perPage}`, {
-    method: 'GET',
-    headers: {
-      'content-type': 'application/json',
-      'token': localStorage.getItem('b2b_token')
-    }
-  });
-  const data = await resp.json();
-  return data;
-};
+  const getProjects1 = async (id, query, page, perPage) => {
+    const resp = await fetch(`${baseUrl}/project/getProjects1?id=${id}&query=${query}&page=${page}&perPage=${perPage}`, {
+      method: 'GET',
+      headers: {
+        'content-type': 'application/json',
+        'token': localStorage.getItem('b2b_token')
+      }
+    });
+    const data = await resp.json();
+    return data;
+  };
 
-const deleteProject = async (id) => {
-  const resp = await fetch(`${baseUrl}/project/deleteProject/${id}`, {
-    method: 'DELETE',
-    headers: {
-      'content-type': 'application/json',
-      'token': localStorage.getItem('dolibo_token')
-    }
-  });
-  const data = await resp.json();
-  return data;
-};
+  const deleteProject = async (id) => {
+    const resp = await fetch(`${baseUrl}/project/deleteProject/${id}`, {
+      method: 'DELETE',
+      headers: {
+        'content-type': 'application/json',
+        'token': localStorage.getItem('dolibo_token')
+      }
+    });
+    const data = await resp.json();
+    return data;
+  };
 
 
-const updateProject = async({title , desc , location , file ,id , bidDate ,
-  startDate,
-  stage,
-  buildingUse,
-  Value,
-  sector})=>{
-
-  const token = localStorage.getItem('b2b_token');
-
-  const data = {
-    title,
-    desc,
-    location,
-    file , 
-    bidDate ,
+  const updateProject = async ({ title, desc, location, file, id, bidDate,
     startDate,
     stage,
     buildingUse,
-    Value ,
-    sector , 
-};
+    Value,
+    sector }) => {
+
+    const token = localStorage.getItem('b2b_token');
+
+    const data = {
+      title,
+      desc,
+      location,
+      file,
+      bidDate,
+      startDate,
+      stage,
+      buildingUse,
+      Value,
+      sector,
+    };
 
 
-try {
-  const resp = await fetch(`${baseUrl}/project/updateProject/${id}`, {
-      method: 'PUT',
-      headers: {
+    try {
+      const resp = await fetch(`${baseUrl}/project/updateProject/${id}`, {
+        method: 'PUT',
+        headers: {
           'Content-Type': 'application/json',
           'Authorization': `Bearer ${token}`
-      },
-      body: JSON.stringify(data)
-  });
+        },
+        body: JSON.stringify(data)
+      });
 
-  if (!resp.ok) {
-      throw new Error('Network response was not ok');
+      if (!resp.ok) {
+        throw new Error('Network response was not ok');
+      }
+
+      return await resp.json();
+    } catch (error) {
+      console.error('There was an error!', error);
+      throw error;
+    }
+
   }
 
-  return await resp.json();
-} catch (error) {
-  console.error('There was an error!', error);
-  throw error;
-}
 
-}
-
-
-const fetchUserCategory = async({category})=>{
-  try {
-    const resp = await fetch(`${baseUrl}/user/getUserByCategory/${category}`, {
+  const fetchUserCategory = async ({ category }) => {
+    try {
+      const resp = await fetch(`${baseUrl}/user/getUserByCategory/${category}`, {
         method: 'GET',
         headers: {
-            'Content-Type': 'application/json',
+          'Content-Type': 'application/json',
         },
-    });
-  
-    if (!resp.ok) {
+      });
+
+      if (!resp.ok) {
         throw new Error('Network response was not ok');
+      }
+
+      return await resp.json();
+    } catch (error) {
+      console.error('There was an error!', error);
+      throw error;
     }
-  
-    return await resp.json();
-  } catch (error) {
-    console.error('There was an error!', error);
-    throw error;
   }
-}
-  
 
-const getProjectDetailById = async({id})=>{
-  const token = localStorage.getItem('b2b_token');
 
-  try{
+  const getProjectDetailById = async ({ id }) => {
+    const token = localStorage.getItem('b2b_token');
 
-    const resp = await fetch(`${baseUrl}/project/getProjectDetail/${id}`, {
-      method: 'GET',
-      headers: {
-        'Authorization': `Bearer ${token}`
-      },
-    });
-    
+    try {
 
-    
-    return await resp.json();
+      const resp = await fetch(`${baseUrl}/project/getProjectDetail/${id}`, {
+        method: 'GET',
+        headers: {
+          'Authorization': `Bearer ${token}`
+        },
+      });
+
+
+
+      return await resp.json();
+    }
+    catch (error) {
+      console.error('There was an error!', error);
+      throw error;
+    }
   }
-  catch(error){
-    console.error('There was an error!', error);
-    throw error;
+
+  const fetchAllLocation = async () => {
+    const token = localStorage.getItem('b2b_token');
+
+    try {
+
+      const resp = await fetch(`${baseUrl}/project/fetchAllLocation`, {
+        method: 'GET',
+        headers: {
+          'Authorization': `Bearer ${token}`
+        },
+      });
+
+      return await resp.json();
+    }
+    catch (error) {
+      console.error('There was an error!', error);
+      throw error;
+    }
   }
-}
 
-const fetchAllLocation = async()=>{
-  const token = localStorage.getItem('b2b_token');
+  const fetchProjectByLoc = async ({ location }) => {
+    const token = localStorage.getItem('b2b_token');
 
-  try{
+    try {
 
-    const resp = await fetch(`${baseUrl}/project/fetchAllLocation`, {
-      method: 'GET',
-      headers: {
-        'Authorization': `Bearer ${token}`
-      },
-    });
-  
-    return await resp.json();
+      const resp = await fetch(`${baseUrl}/project/fetchProjectByLocation`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${token}`
+        },
+        body: JSON.stringify({ location: location })
+      });
+
+      return await resp.json();
+    }
+    catch (error) {
+      console.error('There was an error!', error);
+      throw error;
+    }
   }
-  catch(error){
-    console.error('There was an error!', error);
-    throw error;
-  }
-}
-
-const fetchProjectByLoc = async({location})=>{
-  const token = localStorage.getItem('b2b_token');
-
-  try{
-
-    const resp = await fetch(`${baseUrl}/project/fetchProjectByLocation`, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        'Authorization': `Bearer ${token}`
-      },
-      body: JSON.stringify({location: location})
-    });
-  
-    return await resp.json();
-  }
-  catch(error){
-    console.error('There was an error!', error);
-    throw error;
-  }
-}
 
 
 
@@ -439,7 +439,7 @@ const fetchProjectByLoc = async({location})=>{
 
 
   return (
-    <MainContext.Provider value={{ login,getProjectDetailById,fetchUserCategory, register,updateProject , getUsers, user, setUser, updateUser,verify,sendOtp,submitOtp,changePassword,deleteImage,resetPassword ,projectPostImage , projectDeleteImg ,postProject  , getProjects,deleteProject,getProjects1 , fetchAllLocation , fetchProjectByLoc }}>
+    <MainContext.Provider value={{ login, getProjectDetailById, fetchUserCategory, register, updateProject, getUsers, user, setUser, updateUser, verify, sendOtp, submitOtp, changePassword, deleteImage, resetPassword, projectPostImage, projectDeleteImg, postProject, getProjects, deleteProject, getProjects1, fetchAllLocation, fetchProjectByLoc }}>
       {props.children}
     </MainContext.Provider>
   );
